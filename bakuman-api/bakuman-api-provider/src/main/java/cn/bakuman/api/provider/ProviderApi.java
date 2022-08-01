ef24a853ee341f1ee06d3d7bfa1f06a58a0b9c8b
zhangxiaojian <zhangxiaojian@51iros.com>
2022-08-01 16:19:25 +0800

    配置文件提交
    POM提交
    
    squash! POM提交

--- bakuman-api/bakuman-api-provider/src/main/java/cn/bakuman/api/provider/ProviderApi.java
+++ bakuman-api/bakuman-api-provider/src/main/java/cn/bakuman/api/provider/ProviderApi.java
@@ -4,13 +4,10 @@ import cn.bakuman.base.common.model.R;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Date;
-
 /**
  * @Author 梦叶
  */
-//@FeignClient("bakuman-provider-service")
 public interface ProviderApi {
     @GetMapping("/provider/test")
-    R<Date> test(@RequestParam("str") String str);
+    R<String> test(@RequestParam("str") String str);
 }
